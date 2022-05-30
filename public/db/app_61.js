2d2e962d1527b6925ff7c7909f42c68e8ee275c9
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2022-05-30 18:57:51 -0300

    Add files via upload

--- public/db/app_61.js
+++ public/db/app_61.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3061;
+const port = 3000;
 const sqlite3 = require('sqlite3').verbose();
 const DBPATH = 'curriculo.db';
 
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static("../frontend/"));
+app.use(express.static("../"));
 
 app.use(express.json());
 
@@ -19,12 +19,12 @@ app.use(express.json());
 /****** CRUD ******************************************************************/
 
 // Retorna todos registros (é o R do CRUD - Read)
-app.get('/users', (req, res) => {
+app.get('/getprojects', (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
-  var sql = 'SELECT * FROM tbUser ORDER BY title COLLATE NOCASE';
+   var sql = 'SELECT * FROM projects';
 	db.all(sql, [],  (err, rows ) => {
 		if (err) {
 		    throw err;
@@ -35,12 +35,14 @@ app.get('/users', (req, res) => {
 	db.close(); // Fecha o banco
 });
 
+
+
 // Insere um registro (é o C do CRUD - Create)
-app.post('/userinsert', urlencodedParser, (req, res) => {
+app.post('/insertdata', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-	sql = "INSERT INTO tbUser (title, id, completed) VALUES ('" + req.body.title + "', 33, false)";
+	sql = "INSERT INTO courses(image, name, description, link) values (" + data+")" 
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.run(sql, [],  err => {
 		if (err) {
--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -1,34 +1,8 @@
 // Adicionar novos itens na aba de projetos
 const projects = document.querySelector(".projects_section");
 
-let darkToggle = document.querySelector(".dark");
-let darkMode = document.querySelector(".dark_change");
-var change = 0;
 
-
-load_functions();
-
-
-
-function load_functions(){
-    window.addEventListener('DOMContentLoaded', () => {
-        loadJSON();
-    });
-}
-
-darkToggle.onclick = function() {
-   
-
-    if (change == 0){
-        darkMode.classList.add("dark_mode");
-        change = 1;
-    } else {
-        darkMode.classList.remove("dark_mode");
-        change = 0;
-    }
-}
-
-function loadJSON(){
+function loadProjects(data) {
     fetch('javaScript/data.json')
     .then(response => response.json())
     .then(data => {
@@ -48,4 +22,39 @@ function loadJSON(){
         });
         projects.innerHTML = html;
     })
+}
+
+
+
+function getProjects(){
+    let url = '/getprojects'
+
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("get", url, false)
+    xhttp.send()
+
+    let data = JSON.parse(xhttp.responseText)
+
+    let html = '';
+        data.forEach(element => {
+            console.log(element)
+            html += `
+            <div class="projects">
+                <div class="project">
+                    <img src="${element.image}">
+                    <h1>${element.name}</h1>
+                    <p>${element.description}</p>
+                    <a href="${element.link}" target="_blank"><button type="button" >Acessar projeto</button></a>
+                </div>
+            </div>
+            `;
+
+        });
+        projects.innerHTML = html;
+}
+
+if (window.location.href == "http://localhost:3000/") {
+    getProjects()
+} else {
+    loadProjects()
 }
